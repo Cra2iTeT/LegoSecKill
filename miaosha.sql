403465daf4037755b164d9055b5b85baf904f4b4
Cra2iTeT <71752851+Cra2iTeT@users.noreply.github.com>
2023-01-28 17:55:12 +0800

    Add files via upload

---
+++ miaosha.sql
@@ -0,0 +1,70 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : master
+ Source Server Type    : MySQL
+ Source Server Version : 80026
+ Source Host           : 192.168.150.130:3306
+ Source Schema         : miaosha
+
+ Target Server Type    : MySQL
+ Target Server Version : 80026
+ File Encoding         : 65001
+
+ Date: 28/01/2023 17:32:12
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods`  (
+  `goods_id` bigint(0) NOT NULL,
+  `stock` int(0) NOT NULL COMMENT '商品数量',
+  `start_time` datetime(0) NOT NULL COMMENT '抢购开始时间',
+  `end_time` datetime(0) NOT NULL COMMENT '抢购结束时间',
+  PRIMARY KEY (`goods_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+INSERT INTO `goods` VALUES (1619220879609188352, 50, '2023-01-28 14:29:48', '2023-01-29 14:13:48');
+
+-- ----------------------------
+-- Table structure for t_order
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order`;
+CREATE TABLE `t_order`  (
+  `order_id` bigint(0) NOT NULL,
+  `user_id` bigint(0) NOT NULL,
+  `goods_id` bigint(0) NOT NULL,
+  `order_status` tinyint(0) NOT NULL COMMENT '订单状态（0 等待支付 1 支付完成 2 取消 3 删除）',
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_order
+-- ----------------------------
+INSERT INTO `t_order` VALUES (1619238673881001984, 1618588186685067264, 1619220879609188352, 1);
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `user_id` bigint(0) NOT NULL COMMENT '唯一id',
+  `phone` varchar(13) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '登录手机号',
+  `pwd_md5` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '登录密码',
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1618588186685067264, '15280775219', '123456');
+
+SET FOREIGN_KEY_CHECKS = 1;
